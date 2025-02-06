c61f7d31314149f3abebeb299625e845c3d0f41a
Mehrdad Alemzadeh <zade.mehrdad@gmail.com>
2025-02-06 11:10:39 -0500

    enforce installables for win

--- setup.bat
+++ setup.bat
@@ -10,6 +10,9 @@ call venv\Scripts\activate
 :: Install required packages
 pip install -r requirements.txt
 pip install --upgrade --force-reinstall ollama
+pip install --upgrade gradio
+pip install --upgrade langchain_community
+pip install --upgrade langchain_ollama 
 
 :: Run Ollama with DeepSeek-R1 model
 ollama run deepseek-r1
